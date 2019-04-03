c0d0ad3b0dd8ff11faec79ec89021c3b22dd9f22
Marie <marie@macbook-air-de-marie.home>
2019-04-03 11:58:54 +0200

    message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,52 @@
 class GamesController < ApplicationController
+
+QUESTIONS = [
+   "Est-ce qu'il a les yeux bleus ?",
+   "Est-ce que c'est un homme ?",
+   "Est-ce qu'il est vegan ?",
+   "Est-ce qu'il vient d'Aquitaine ?"
+  ]
+
+student1 = {
+    "name" => "Marie",
+    "answers" => ["yes", "no", "no", "no"]
+  }
+  student2 = {
+    "name" => "Roxane",
+    "answers" => ["yes", "no", "no", "yes"]
+  }
+  STUDENTS = [ student1, student2 ]
+
+  def show
+    @game_id = params["id"]
+    @game = Game.find(@game_id)
+    @answers = @game["answers"]
+    @students = STUDENTS
+  end
+
+
   def create
-  	redirect_to edit_game_path(1), method: :get
+    @game = Game.create
+  	redirect_to edit_game_path(id: @game["id"], question_id: 0), method: :get
   end
 
   def edit
+    @game_id = params["id"]
+    @question_id = params["question_id"].to_i
+    @question = QUESTIONS[@question_id]
   end
 
   def update
-  	redirect_to game_path(1), method: :get
+    @game_id = params["id"]
+    @game = Game.find(@game_id)
+    @game["answers"] << params["answer"]
+    @game.save
+  	@question_id = params["question_id"].to_i
+    if @question_id < QUESTIONS.size
+      redirect_to edit_game_path(id: @game_id, question_id: @question_id), method: :get
+    else
+      redirect_to game_path(id: @game_id), method: :get
+    end
+  end
   end
 
-  def show
-  end
-end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,19 +7,19 @@ class PagesController < ApplicationController
  article1 = 
 	{ 
 	"title" => "Mon 1er article",
-	"content" => "blablablablabla"
+	"content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 } 
 
  article2 = 
 	{ 
 	"title" => "Mon 2ème article",
-	"content" => "blablablablabla"
+	"content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 } 
 
 article3 = 
 	{ 
 	"title" => "Mon 3ème article",
-	"content" => "blablablablabla"
+	"content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 } 
 
 @articles = [article1, article2, article3] 
